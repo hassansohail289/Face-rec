f53df6c0bee2052277a509e04674be4eba3d0d9a
Hassan Sohail <hassansohail289@gmail.com>
2026-03-12 11:40:11 +0000

    fourt commit

--- main.py
+++ main.py
@@ -1,104 +1,80 @@
-import cv2
 import os
+import torch
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+os.environ['CUDA_VISIBLE_DEVICES'] = '1'
+os.environ['TORCH_CUDA_ARCH_LIST'] ='8.9' 
+
+import cv2
 import shutil
 from deepface import DeepFace
-import yt_dlp
+import tensorflow as tf
+
+gpus = tf.config.list_physical_devices('GPU')
+if gpus:
+    try:
+        
+        for gpu in gpus:
+            tf.config.experimental.set_memory_growth(gpu, True)
+        tf.config.set_visible_devices(gpus[0], 'GPU')
+        print(f"[SUCCESS] RTX  Activated! Loading models to VRAM...")
+    except Exception as e:
+        print(f"[GPU ERROR] {e}")
+else:
+    print("[WARNING] NVIDIA GPU not detected by TensorFlow. Check CUDA drivers.")
 
 
-USE_LOCAL_VIDEO = False
-LOCAL_VIDEO_PATH = "video2.mp4" 
-
-VIDEO_URL = "https://youtu.be/QE3QwTA5ujE?si=JiiX0X6aQpYZtOEt" 
-REFERENCE_IMG = "reference.jpg" 
+USE_LOCAL_VIDEO = True
+LOCAL_VIDEO_PATH = "video.mp4" 
+REFERENCE_IMG = "video.jpg" 
 OUTPUT_DIR = "ai_perfect_shots"
-TEMP_VIDEO = "temp_interview.mp4"
+FRAME_SKIP = 200
 
 def setup_clean_folders():
     if os.path.exists(OUTPUT_DIR):
-        print(f"[INFO] Deleting old shots from '{OUTPUT_DIR}'...")
         shutil.rmtree(OUTPUT_DIR)
     os.makedirs(OUTPUT_DIR)
-    
-    if not USE_LOCAL_VIDEO and os.path.exists(TEMP_VIDEO):
-        print("[INFO] Removing old temporary video...")
-        os.remove(TEMP_VIDEO)
-
-def download_video(url):
-    print("[INFO] Downloading new video segment from YouTube...")
-    ydl_opts = {
-        'format': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/mp4',
-        'outtmpl': TEMP_VIDEO,
-        'quiet': False,
-        'no_warnings': True,
-    }
-    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        ydl.download([url])
 
 def run_project():
     setup_clean_folders()
-    
-    
-    video_source = ""
-    if USE_LOCAL_VIDEO:
-        if os.path.exists(LOCAL_VIDEO_PATH):
-            video_source = LOCAL_VIDEO_PATH
-            print(f"[INFO] Using local video: {video_source}")
-        else:
-            print(f"[ERROR] Local video '{LOCAL_VIDEO_PATH}' not found!")
-            return
-    else:
-        download_video(VIDEO_URL)
-        video_source = TEMP_VIDEO
-
-    cap = cv2.VideoCapture(video_source)
+    cap = cv2.VideoCapture(LOCAL_VIDEO_PATH)
     saved = 0
     frame_idx = 0
 
-    print(f"\n[INFO] Starting AI Scanning on: {video_source}")
+    print(f"\n[GPU MINING] Scanning video using NVIDIA Cuda Cores...")
 
-    while cap.isOpened() and saved < 10:
+    while cap.isOpened():
         ret, frame = cap.read()
         if not ret: break
 
-        if frame_idx % 80 == 0:
+        if frame_idx % FRAME_SKIP == 0:
             try:
-                
-                result = DeepFace.verify(
+                verify = DeepFace.verify(
                     img1_path = frame, 
                     img2_path = REFERENCE_IMG, 
                     model_name = "Facenet", 
-                    detector_backend = "opencv",
-                    distance_metric = "cosine",
-                    enforce_detection = False, 
-                    align = False
+                    detector_backend = "retinaface",
+                    enforce_detection = False
                 )
-                
-                if result['distance'] < 0.25: 
-                    
+
+                if verify['distance'] < 0.25:
                     analysis = DeepFace.analyze(
                         img_path = frame, 
                         actions = ['emotion'], 
-                        detector_backend = 'opencv', 
+                        detector_backend = 'retinaface', 
                         enforce_detection = False,
                         silent = True
                     )
                     
-                    emotion = analysis[0]['dominant_emotion']
-                    
-                    if emotion in ['neutral', 'happy']:
-                       
-                        print(f"[PERFECT MATCH] Frame {frame_idx}: Score {result['distance']:.4f} | Emotion: {emotion}")
-                        cv2.imwrite(f"{OUTPUT_DIR}/perfect_shot_{saved+1}.jpg", frame)
+                    if analysis[0]['dominant_emotion'] in ['neutral', 'happy']:
                         saved += 1
+                        print(f"[MATCH] Shot {saved} found at frame {frame_idx}")
+                        cv2.imwrite(f"{OUTPUT_DIR}/perfect_shot_{saved}.jpg", frame)
             except:
                 pass
-        
         frame_idx += 1
-        if frame_idx % 400 == 0:
-            print(f"[PROGRESS] Scanning frames... {frame_idx}")
-
+    
     cap.release()
-    print(f"\n[DONE] Professional FULL shots saved in '{OUTPUT_DIR}'.")
+    print(f"\n[DONE] Total {saved} shots saved.")
 
 if __name__ == "__main__":
     run_project()
